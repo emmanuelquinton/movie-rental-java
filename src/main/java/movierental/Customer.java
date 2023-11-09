c75d450a26220de943018ba262a74286508043c6
Emmanuel <equinton.pro@gmail.com>
2023-11-09 07:24:03 +0100

    Split the loop in 2 loops to distinguish the responsabilities

--- src/main/java/movierental/Customer.java
+++ src/main/java/movierental/Customer.java
@@ -45,15 +45,23 @@ public class Customer {
                     break;
             }
 
+
+            // show figures for this rental
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+            totalAmount += thisAmount;
+        }
+
+        for (Rental each : _rentals) {
+
+
+
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
                 frequentRenterPoints++;
 
-            // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+
         }
 
         // add footer lines
